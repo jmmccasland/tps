a4697459abf15ad03edcf1c798580e44ae18aeed
John Michael McCasland <jmmccasland@gmail.com>
2017-05-31 14:21:53 -0500

    Install jQuery, fadeIn effect unfinished

--- app/index.js
+++ app/index.js
@@ -1,1 +1,15 @@
 import 'whatwg-fetch';
+import $ from 'jQuery';
+
+// var $ = require('jquery');
+
+$(document).ready(() => {
+  setTimeout(() => {
+    $('body')
+    .css('background-color', '#f1f3f7')
+    .css('opacity', 1);
+
+    // $('.container')
+    //   .css('border-top', 0);
+  }, 1000);
+});
