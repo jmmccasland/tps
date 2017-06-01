9b772b4b12705b4a6f56314c51f90dfc33c2b3a4
John Michael McCasland <jmmccasland@gmail.com>
2017-05-31 20:39:01 -0500

    Base mobile styles

--- app/index.js
+++ app/index.js
@@ -1,15 +1,9 @@
 import 'whatwg-fetch';
 import $ from 'jQuery';
 
-// var $ = require('jquery');
-
 $(document).ready(() => {
   setTimeout(() => {
-    $('body')
-    .css('background-color', '#f1f3f7')
-    .css('opacity', 1);
-
-    // $('.container')
-    //   .css('border-top', 0);
+    $('.loading')
+    .fadeOut();
   }, 1000);
 });
